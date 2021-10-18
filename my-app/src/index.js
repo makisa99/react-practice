24c4495fc48ec7b757bde4d4ad844aa1b2c748c0
makisa99 <72605856+makisa99@users.noreply.github.com>
2021-10-18 17:46:32 +0200

    iterating over string array (map)

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,31 +3,50 @@ import ReactDom from "react-dom";
 
 //Styling
 import "./index.css";
-// setup vars
-const firstBook = {
-  author: "Ann Whitford Paul",
-  title: "If Animals Kissed Good Night",
-  img: "https://images-na.ssl-images-amazon.com/images/I/81nzxODnaJL._AC_UL200_SR200,200_.jpg",
-};
+// setup vars - list
+const books = [
+  {
+    author: "Ann Whitford Paul",
+    title: "If Animals Kissed Good Night",
+    img: "https://images-na.ssl-images-amazon.com/images/I/81nzxODnaJL._AC_UL200_SR200,200_.jpg",
+  },
+  {
+    author: "It Ends with Us: A Novel",
+    title: "Colleen Hoover",
+    img: "https://images-na.ssl-images-amazon.com/images/I/71xUvuJiqgL._AC_UL200_SR200,200_.jpg",
+  },
+];
+const names = ["Mario", "Nemanja", "Dragan"];
+const newNames = names.map((name) => {
+  return <h1>{name}</h1>;
+});
+console.log(newNames);
 function BookList() {
   return (
     <section className="booklist">
-      <Book
+      {newNames}
+      {/* <Book
         img={firstBook.img}
         title={firstBook.title}
         author={firstBook.author}
-      />
+      ></Book>
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      /> */}
     </section>
   );
 }
 
-const Book = (props) => {
-  console.log(props);
+//gettings props
+const Book = ({ img, title, author }) => {
   return (
     <article className="book">
-      <img src={props.img} alt="book img" />
-      <h1>{props.title}</h1>
-      <h4>{props.author}</h4>
+      <img src={img} alt="book img" />
+      <h1>{title}</h1> {/* props.title */}
+      <h4>{author}</h4>
+      {/* {children} props.children */}
     </article>
   );
 };
