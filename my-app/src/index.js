9a88d392d021fcba3ea3f9e00bbd9d0c28a410d1
makisa99 <72605856+makisa99@users.noreply.github.com>
2021-10-18 13:47:11 +0200

    props-beginning

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,42 +3,35 @@ import ReactDom from "react-dom";
 
 //Styling
 import "./index.css";
-
+// setup vars
+const firstBook = {
+  author: "Ann Whitford Paul",
+  title: "If Animals Kissed Good Night",
+  img: "https://images-na.ssl-images-amazon.com/images/I/81nzxODnaJL._AC_UL200_SR200,200_.jpg",
+};
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
     </section>
   );
 }
 
-const Book = () => {
+const Book = (props) => {
+  console.log(props);
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img src={props.img} alt="book img" />
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
     </article>
   );
 };
 
-const Image = () => (
-  <img
-    src="https://images-na.ssl-images-amazon.com/images/I/81nzxODnaJL._AC_UL200_SR200,200_.jpg"
-    alt="img"
-  />
-);
-
-const Title = () => <h1>If Animals Kissed Good Night</h1>;
-const Author = () => <h4>Ann Whitford Paul</h4>;
 ReactDom.render(<BookList />, document.getElementById("root"));
 
 //Fix for hot reloading
