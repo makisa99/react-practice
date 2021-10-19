2b20277c343eb2628d80c3981e1f016149e07071
makisa99 <72605856+makisa99@users.noreply.github.com>
2021-10-19 18:20:12 +0200

    useState arrays and objects

--- react-advanced/src/tutorial/1-useState/setup/4-useState-object.js
+++ react-advanced/src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: "Mario",
+    age: "22",
+    message: "random msg",
+  });
+  console.log(person);
+  const changeMessage = () => {
+    setPerson({ ...person, message: "Hello world" });
+  };
+  return (
+    <>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>{person.message}</h3>
+      <button className="btn" onClick={changeMessage}>
+        change message
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
