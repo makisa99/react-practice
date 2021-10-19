7fc2eba86e447378e3d965c6a5a22869f3740e7f
makisa99 <72605856+makisa99@users.noreply.github.com>
2021-10-19 02:17:41 +0200

    click and hover

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -6,46 +6,56 @@ import "./index.css";
 // setup vars - list
 const books = [
   {
+    id: 1,
     author: "Ann Whitford Paul",
     title: "If Animals Kissed Good Night",
     img: "https://images-na.ssl-images-amazon.com/images/I/81nzxODnaJL._AC_UL200_SR200,200_.jpg",
   },
   {
+    id: 2,
     author: "It Ends with Us: A Novel",
     title: "Colleen Hoover",
     img: "https://images-na.ssl-images-amazon.com/images/I/71xUvuJiqgL._AC_UL200_SR200,200_.jpg",
   },
+  {
+    id: 3,
+    author: "Little Blue Truck's Halloween",
+    title: "Alice Schertle",
+    img: "https://images-na.ssl-images-amazon.com/images/I/51MB5SD5GIL._AC_UL200_SR200,200_.jpg",
+  },
 ];
-const names = ["Mario", "Nemanja", "Dragan"];
-const newNames = names.map((name) => {
-  return <h1>{name}</h1>;
-});
-console.log(newNames);
+
 function BookList() {
   return (
     <section className="booklist">
-      {newNames}
-      {/* <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      ></Book>
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      /> */}
+      {books.map((book) => {
+        return <Book key={book.id} {...book}></Book>;
+      })}
     </section>
   );
 }
 
-//gettings props
+//getting props
 const Book = ({ img, title, author }) => {
+  // const { img, title, author } = props;
+  //attribute, eventHandler
+  // onClick, onMouseOver
+  const clickHandler = () => {
+    alert("Hello world");
+  };
   return (
-    <article className="book">
+    <article
+      className="book"
+      onMouseOver={() => {
+        console.log("HOVER");
+      }}
+    >
       <img src={img} alt="book img" />
       <h1>{title}</h1> {/* props.title */}
       <h4>{author}</h4>
+      <button type="button" onClick={clickHandler}>
+        ref ex
+      </button>
       {/* {children} props.children */}
     </article>
   );
